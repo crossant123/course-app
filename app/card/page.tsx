da81afaba0b2c8a261e4fe1ede63a92604a3d9f1
Anthony <crossant123@gmail.com>
2023-11-15 11:23:25 -0500

    added business card side project to fully style

---
+++ app/card/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <section>
+      <div id='Business-Card' className='container'>
+        <div id='Photo-board'>Photo card</div>
+        <div id='Contact-card'>Contact Card</div>
+        <div id='Social-Card'>Social Card</div>
+        <div id='info-container'>
+          <div id='Name-Card'>Name Card</div>
+          <div id='Bio-Card'>Bio Card</div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default page
