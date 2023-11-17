c88915004d63e736098b0b179b0cbdfcc356fa5e
Anthony <crossant123@gmail.com>
2023-11-17 09:16:35 -0500

    Continued work on card styles

--- app/card/page.tsx
+++ app/card/page.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const page = () => {
   return (
-    <section className='container relative '>
-      <div
-        id='Business-Card'
-        className='relative m-4 flex h-96 rounded-lg bg-teal-500 p-4'
-      >
+    <section className='flex justify-evenly'>
+      <div id='Business-Card' className='bg-teal-100'>
         <div
           id='Photo-board'
-          className='left-30 absolute top-0 w-40 bg-emerald-400'
+          className='grid justify-center gap-4 overflow-hidden rounded-lg bg-teal-400 p-4  md:rounded-md'
         >
-          <p>image head</p>
+          <h1 className=' font-bold'>Anthony Cross</h1>
           <Image
             src='/portrait-image.jpg'
             width={180}
@@ -20,16 +18,44 @@ const page = () => {
             alt='Picture of Author'
           ></Image>
         </div>
-        <div id='Contact-card'>Contact Card</div>
-        <div id='Social-Card'>Social Card</div>
-        <div
-          id='info-container'
-          className=' flex-1 overflow-hidden rounded-lg bg-white'
-        >
-          <div id='Name-Card' className=' bg-teal-200'>
+        <div id='Contact-card' className='mt-2 rounded-lg bg-emerald-200 p-4'>
+          <h2 className='font-bold'>Contact</h2>
+          <p>The best ways to contact me:</p>
+
+          <ul className='mt-2 flex flex-row gap-4'>
+            <li>
+              <h3 className='font-semibold'>Email</h3>
+              <p>crossant123@gmail.com</p>
+              <button>Contact Us</button>
+            </li>
+            <li>
+              <h3 className=' font-semibold'>Phone</h3>
+              <p>(616)250-7374</p>
+              <button>Contact Us</button>
+            </li>
+            <li>
+              <h3 className=' font-semibold'>Alternative Contact</h3>
+              <ul>
+                <li>
+                  <h4>Linkedin</h4>
+                  <button>
+                    <Link href='https://www.linkedin.com/in/anthonycross123/'>
+                      Contact Us
+                    </Link>
+                  </button>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <div id='Social-Card' className='p-4 font-bold'>
+          Social Card
+        </div>
+        <div id='info-container' className='p-4 '>
+          <div id='Name-Card' className=''>
             Anthony Cross
           </div>
-          <div id='Bio-Card' className='bg-teal-100'>
+          <div id='Bio-Card' className=''>
             Fill with text about myself
           </div>
         </div>
