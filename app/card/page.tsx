eb208346efe3525dc3db665874da433844de5c9c
Anthony <crossant123@gmail.com>
2023-11-22 19:09:05 -0500

    Finalized detail and created svg social icons link

--- app/card/page.tsx
+++ app/card/page.tsx
@@ -11,7 +11,7 @@ const page = () => {
       >
         <div
           id='contact-section'
-          className='flex h-full w-1/3 flex-col gap-2 bg-green-600 p-4'
+          className='flex h-full w-1/3 flex-col gap-2 bg-green-600'
         >
           {/* contact Top Info Section */}
           <div className='mt-80 flex flex-col  px-12 text-white'>
@@ -40,29 +40,48 @@ const page = () => {
               <p>(616)250-7374</p>
             </div>
           </div>
+          <div
+            id='Separator bar'
+            className='mt-8 min-h-[20px] min-w-full bg-white'
+          />
 
           {/* Left side Footer - Social Section */}
-          <div className='mt-40 px-12'>
-            <h2 className='text-white'>Social Media</h2>
+          <div className='mt-32 px-12'>
+            <h2 className='mb-2 text-white'>Social Media</h2>
             <div className='flex flex-row gap-2'>
-              <Image
-                src={'/phone.svg'}
-                width={25}
-                height={25}
-                alt='mail icon'
-              />
-              <Image
-                src={'/phone.svg'}
-                width={25}
-                height={25}
-                alt='mail icon'
-              />
-              <Image
-                src={'/phone.svg'}
-                width={25}
-                height={25}
-                alt='mail icon'
-              />
+              <Link
+                href={'https://www.facebook.com/crossant123'}
+                target='_blank'
+              >
+                <Image
+                  src={'/facebook.svg'}
+                  width={25}
+                  height={25}
+                  alt='Facebook icon'
+                />
+              </Link>
+              <Link
+                href={'https://www.threads.net/@crossant_official'}
+                target='_blank'
+              >
+                <Image
+                  src={'/threads.svg'}
+                  width={25}
+                  height={25}
+                  alt='Threads icon'
+                />
+              </Link>
+              <Link
+                href={'https://www.linkedin.com/in/anthonycross123/'}
+                target='_blank'
+              >
+                <Image
+                  src={'/linkedin.svg'}
+                  width={25}
+                  height={25}
+                  alt='Linkedin icon'
+                />
+              </Link>
             </div>
           </div>
         </div>
